ebbdf7afadbdec636e3e2fd2e6783692af4a40cd
laindomJS <adrijar21@gmail.com>
2023-03-06 13:59:56 -0400

    feat: added fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+
+  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_REACT_API_KEY}&q=panda&limit=10&offset=0&rating=g&lang=en`;
+
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        setGifs(data.data);
+        console.log(data.data)
+      })
+      .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      {
+        gifs.map(gif => (
+          <img src={gif.images.downsized_medium.url} alt={gif.title} />
+        ))
+      }
     </div>
   )
 }
