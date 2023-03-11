145f63ad690bc7be8491b99cb44c9809da8dc1f4
laindomJS <adrijar21@gmail.com>
2023-03-11 16:08:30 -0400

    styles: corrected grid styles

--- src/components/ListOfGifs/ListOfGifs.jsx
+++ src/components/ListOfGifs/ListOfGifs.jsx
@@ -2,7 +2,7 @@ import { Gif } from '../Gif/Gif';
 import uuid from 'react-uuid';
 
 export function ListOfGifs ({gifs}) {
-  return <div className='min-h-screen flex flex-col justify-center items-center space-y-8 p-4 md:flex-wrap md:flex-row md:space-x-8'>
+  return <div className='min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8 gap-4 items-center'>
     {
       gifs.map((gif) =>
         <Gif key={uuid()} {...gif} />
